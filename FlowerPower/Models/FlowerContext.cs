0723e3d3f349abb4c81c076dc9a181ec95c51cf7
Adrian Vladu <adyv_adyv@yahoo.com>
2012-12-15 03:53:09 +0200

    pass changed

---
+++ FlowerPower/Models/FlowerContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FlowerPower.Models
+{
+    public class FlowerContext : DbContext
+    {
+        public FlowerContext()
+            : base("FlowerConnection")
+        {
+        }
+        public DbSet<Flower> Flowers { get; set; }
+    }
+}
---
+++ FlowerPower/Scripts/App/Router.js
@@ -0,0 +1,22 @@
+﻿define([
+  // Application.
+  "App",
+  'backbone'
+],
+
+function (app, Backbone) {
+
+    // Defining the application router, you can attach sub routers here.
+    var Router = Backbone.Router.extend({
+        routes: {
+            "": "index"
+        },
+
+        index: function () {
+
+        }
+    });
+
+    return Router;
+
+});
