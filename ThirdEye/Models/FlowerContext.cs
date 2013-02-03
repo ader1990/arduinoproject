e1c6f0687106476a1bff18c3ae62c092fe89b947
ClaudiuNesa <claudiu.nesa@gmail.com>
2013-02-03 10:51:02 +0200

    Adding ThirdEye project folder

---
+++ ThirdEye/Models/FlowerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ThirdEye.Models
+{
+    public class FlowerContext : DbContext
+    {
+        public FlowerContext()
+            : base("FlowerConnection")
+        {
+        }
+        public DbSet<Flower> Flowers { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
